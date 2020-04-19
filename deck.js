f4c8e8ae3d395c06b50d22e164d3c0b7f839e1a9
Tyler Hueter <tylerhueter08@gmail.com>
2020-04-19 17:16:42 -0400

    initial setup and styling

---
+++ deck.js
@@ -0,0 +1,22 @@
+class Deck  {
+    constructor() {
+        this.templateCardDeck = [
+            { pic: "", faceup: false, matched: false},
+            { pic: "", faceup: false, matched: false},
+            { pic: "", faceup: false, matched: false},
+            { pic: "", faceup: false, matched: false },
+            { pic: "", faceup: false, matched: false },
+            { pic: "", faceup: false, matched: false },
+            { pic: "", faceup: false, matched: false },
+            { pic: "", faceup: false, matched: false },
+            { pic: "", faceup: false, matched: false },
+            { pic: "", faceup: false, matched: false },
+            { pic: "", faceup: false, matched: false },
+            { pic: "", faceup: false, matched: false },
+            { pic: "", faceup: false, matched: false },
+            { pic: "", faceup: false, matched: false },
+            { pic: "", faceup: false, matched: false },
+            { pic: "", faceup: false, matched: false }
+        ]
+    }
+}
