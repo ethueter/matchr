31112a8b8b9cc63d346156ef4c91459b8f3bf89a
Tyler Hueter <tylerhueter08@gmail.com>
2020-04-19 23:14:04 -0400

    create deck

--- deck.js
+++ deck.js
@@ -1,22 +1,46 @@
 class Deck  {
     constructor() {
         this.templateCardDeck = [
-            { pic: "", faceup: false, matched: false},
-            { pic: "", faceup: false, matched: false},
-            { pic: "", faceup: false, matched: false},
-            { pic: "", faceup: false, matched: false },
-            { pic: "", faceup: false, matched: false },
-            { pic: "", faceup: false, matched: false },
-            { pic: "", faceup: false, matched: false },
-            { pic: "", faceup: false, matched: false },
-            { pic: "", faceup: false, matched: false },
-            { pic: "", faceup: false, matched: false },
-            { pic: "", faceup: false, matched: false },
-            { pic: "", faceup: false, matched: false },
-            { pic: "", faceup: false, matched: false },
-            { pic: "", faceup: false, matched: false },
-            { pic: "", faceup: false, matched: false },
-            { pic: "", faceup: false, matched: false }
-        ]
+          { pic: "fa fa-camera-retro", faceup: false, matched: false },
+          { pic: "fa fa-camera-retro", faceup: false, matched: false },
+          { pic: "fa fa-hackaday", faceup: false, matched: false },
+          { pic: "fa fa-hackaday", faceup: false, matched: false },
+          { pic: "fa fa-bolt", faceup: false, matched: false },
+          { pic: "fa fa-bolt", faceup: false, matched: false },
+          { pic: "fa fa-balance-scale", faceup: false, matched: false },
+          { pic: "fa fa-balance-scale", faceup: false, matched: false },
+          { pic: "fa fa-paw", faceup: false, matched: false },
+          { pic: "fa fa-paw", faceup: false, matched: false },
+          { pic: "fa fa-trophy", faceup: false, matched: false },
+          { pic: "fa fa-trophy", faceup: false, matched: false },
+          { pic: "fa fa-rocket", faceup: false, matched: false },
+          { pic: "fa fa-rocket", faceup: false, matched: false },
+          { pic: "fa fa-star", faceup: false, matched: false },
+          { pic: "fa fa-star", faceup: false, matched: false }
+        ];
     }
-}
+
+    cardMatch(cardDeck, firstCardIndex, secondCardIndex) {
+        if (cardDeck[firstCardIndex] == cardDeck[secondCardIndex]) {
+            return true
+        } else {
+            return false
+        };
+    };
+
+    shuffleDeck(cardDeck) {
+        const topRange = 16,
+              bottomRange = 1,
+              shuffledDeck = [];
+
+        while (shuffledDeck.length < topRange) {
+            let newIndex = Math.floor(Math.random() * (topRange - bottomRange) + bottomRange);
+            if (shuffledDeck.indexOf(newIndex) == -1 ) {
+                shuffledDeck.push(cardDeck[newIndex])
+            }
+        }
+        return shuffledDeck;
+    };
+};
+
+export default Deck;
---
+++ index.js
@@ -0,0 +1,9 @@
+import Deck from './Deck';
+
+
+const newGame = () => {
+
+    let deckOfCards = new Deck;
+
+
+}
